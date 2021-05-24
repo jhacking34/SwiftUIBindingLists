6d2a8b0c228142482afa0bf3c004414058b764a2
Johnny Hacking <jhacking@bhico.com>
2021-05-24 06:42:22 -0600

    Initial Commit

---
+++ SwiftUIBuildingLists/SwiftUIBuildingListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBuildingListsApp.swift
+//  SwiftUIBuildingLists
+//
+//  Created by Johnny Hacking on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBuildingListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
